89bcc44e6fc898134a9fe5243fd462120484834c
gyorutan <waou1998@gmail.com>
2024-01-27 09:22:39 +0900

    update

--- app/(root)/(routes)/(home)/_components/greeting.tsx
+++ app/(root)/(routes)/(home)/_components/greeting.tsx
@@ -1,20 +1,10 @@
 "use client";
 
-import { api } from "@/lib/api";
 import useAuth from "@/store/useAuth";
-import axios from "axios";
-import React, { useEffect } from "react";
+import React from "react";
 
 const Greeting = () => {
   const auth = useAuth();
-  useEffect(() => {
-    const cookie = async () => {
-      const data = await axios.get(`${api}/users/profile`, {
-        withCredentials: true,
-      });
-    };
-    cookie();
-  }, []);
   return (
     <>
       <p className="text-lg font-bold mb-4 ml-1">😊 {auth.name}님 환영합니다</p>
